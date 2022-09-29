c3c54c255194ecfb28e17cae7b777696bd5072cb
Danendra Clevero Ananda <danendraclever24@gmail.com>
2022-09-29 14:11:46 +0700

    Ball and field completed

--- src/vision_capture.cpp
+++ src/vision_capture.cpp
@@ -16,65 +16,139 @@
 using namespace cv;
 using namespace std;
 
+//---Mutex
+//========
 boost::mutex mutex_field_raw_threshold;
 
-Mat field_raw_threshold = Mat::zeros(Size(g_res_y, g_res_x), CV_8UC1);
+//---Matrix
+//==========
+Mat field_raw_threshold;
+Mat vision_capture_raw;
+Mat vision_capture_rgb;
+Mat vision_capture_yuv;
+
+//---Timer
+//========
+ros::Timer tim_50_hz;
+
+//---Prototypes
+//=============
+void CllbkTim50Hz(const ros::TimerEvent &event);
+
+//---Publisher
+//============
+image_transport::Publisher pub_frame_bgr;
+image_transport::Publisher pub_frame_yuv;
+image_transport::Publisher pub_frame_field_yuv;
+
+//---Vid Capture
+//==============
+// VideoCapture cap("/home/danendra/Iris/jiancuk_raceto/src/vision/vid/percobaan.mkv");
+VideoCapture cap("/dev/v4l/by-id/usb-046d_C922_Pro_Stream_Webcam_7C21B0EF-video-index0");
+
+//---Thresh
+//=========
+int yMin = 93;
+int yMax = 175;
+int uMin = 127;
+int uMax = 255;
+int vMin = 0;
+int vMax = 98;
+
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "vision_capture");
 
     ros::NodeHandle nh;
+    ros::MultiThreadedSpinner MTS;
     image_transport::ImageTransport IT(nh);
     // Todo:
     // 1. Apa yang akan terjadi jika hanya ada yuv yang di pub
-    image_transport::Publisher pub_frame_bgr = IT.advertise("/vision_frame_bgr", 32);
-    image_transport::Publisher pub_frame_yuv = IT.advertise("/vision_frame_yuv", 32);
+    pub_frame_bgr = IT.advertise("/vision_frame_bgr", 32);
+    pub_frame_yuv = IT.advertise("/vision_frame_yuv", 32);
+    pub_frame_field_yuv = IT.advertise("/vision_yuv", 32);
 
-    Mat vision_capture_raw;
-    Mat vision_capture_rgb;
-    VideoCapture cap("/home/danendra/Iris/jiancuk_raceto/src/vision/vid/percobaan.mkv");
-    if (!cap.isOpened())
-    {
-        cout << "No video stream detected" << endl;
-        system("pause");
-        return -1;
-    }
-    while (ros::ok())
-    {
-        cap >> vision_capture_rgb;
-        // cap >> vision_capture_raw;
+    tim_50_hz = nh.createTimer(ros::Duration(0.02), CllbkTim50Hz);
+    // while (ros::ok())
+    // {
+    //     cap >> vision_capture_rgb;
+    //     // cap >> vision_capture_raw;
 
-        if (vision_capture_rgb.empty())
-            break;
+    //     if (vision_capture_rgb.empty())
+    //         break;
 
-        char c = (char)waitKey(25);
+    //     char c = (char)waitKey(25);
 
-        if (c == 27)
-            break;
+    //     if (c == 27)
+    //         break;
 
-        flip(vision_capture_rgb, vision_capture_rgb, 1);
-        resize(vision_capture_rgb, vision_capture_rgb, Size(g_res_y, g_res_x));
-        rotate(vision_capture_rgb, vision_capture_rgb, ROTATE_90_CLOCKWISE);
+    //     flip(vision_capture_rgb, vision_capture_rgb, 1);
+    //     resize(vision_capture_rgb, vision_capture_rgb, Size(g_res_y, g_res_x));
+    //     rotate(vision_capture_rgb, vision_capture_rgb, ROTATE_90_CLOCKWISE);
 
-        // vision_capture_rgb = vision_capture_raw(Rect(0, 0, g_res_y * 0.9, g_res_x * 0.9));
+    //     // vision_capture_rgb = vision_capture_raw(Rect(0, 0, g_res_y * 0.9, g_res_x * 0.9));
 
-        // imshow("Vision Capture", vision_capture_rgb);
+    //     // imshow("Vision Capture", vision_capture_rgb);
 
-        sensor_msgs::ImagePtr msg_frame_bgr =
-            cv_bridge::CvImage(std_msgs::Header(), "bgr8", vision_capture_rgb).toImageMsg();
-        pub_frame_bgr.publish(msg_frame_bgr);
+    //     sensor_msgs::ImagePtr msg_frame_bgr =
+    //         cv_bridge::CvImage(std_msgs::Header(), "bgr8", vision_capture_rgb).toImageMsg();
+    //     pub_frame_bgr.publish(msg_frame_bgr);
 
-        Mat frame_yuv;
-        cvtColor(vision_capture_rgb, frame_yuv, CV_BGR2YUV);
-        sensor_msgs::ImagePtr msg_frame_yuv =
-            cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame_yuv).toImageMsg();
-        pub_frame_yuv.publish(msg_frame_yuv);
+    //     Mat frame_yuv;
+    //     cvtColor(vision_capture_rgb, frame_yuv, CV_BGR2YUV);
+    //     sensor_msgs::ImagePtr msg_frame_yuv =
+    //         cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame_yuv).toImageMsg();
+    //     pub_frame_yuv.publish(msg_frame_yuv);
 
-        ros::spinOnce();
-    }
-    ros::spin();
+    //     ros::spinOnce();
+    // }
+    // ros::spin();
 
-    cap.release();
-    return 0;
+    // cap.release();
+    MTS.spin();
+}
+
+void CllbkTim50Hz(const ros::TimerEvent &event)
+{    
+    //--Store the captured to matrix
+    //==============================
+    cap >> vision_capture_rgb;
+
+    // vision_capture_raw = vision_capture_rgb.clone();
+
+    //---Flip
+    //=======
+    flip(vision_capture_rgb, vision_capture_rgb, 1);
+    resize(vision_capture_rgb, vision_capture_rgb, Size(g_res_y, g_res_x));
+    rotate(vision_capture_rgb, vision_capture_rgb, ROTATE_90_CLOCKWISE);
+
+    cvtColor(vision_capture_rgb, vision_capture_yuv, CV_BGR2YUV);
+
+    namedWindow("view", WINDOW_AUTOSIZE);
+    // createTrackbar("yMin", "view", &yMin, 255);
+    // createTrackbar("yMax", "view", &yMax, 255);
+    // createTrackbar("uMin", "view", &uMin, 255);
+    // createTrackbar("uMax", "view", &uMax, 255);
+    // createTrackbar("vMin", "view", &vMin, 255);
+    // createTrackbar("vMax", "view", &vMax, 255);
+
+    // imshow("view", vision_capture_yuv);
+    // imshow("view2", field_raw_threshold);
+    // imshow("view3", vision_capture_rgb);
+    // waitKey(1);
+
+    //---Publish Field Only
+    //=====================
+    sensor_msgs::ImagePtr msg_frame_field_yuv =
+        cv_bridge::CvImage(std_msgs::Header(), "bgr8", vision_capture_yuv).toImageMsg();
+    pub_frame_field_yuv.publish(msg_frame_field_yuv);
+
+
+    
+    // //---Field
+    // //========
+    // mutex_field_raw_threshold.lock();
+    
+    // mutex_field_raw_threshold.unlock();
 }
--- src/vision_main.cpp
+++ src/vision_main.cpp
@@ -1,7 +1,7 @@
 /**
  * TODO:
  *  - get ball [v]
- *  - get field []
+ *  - get field [v]
  *  - get obs []
  * */
 
@@ -24,7 +24,7 @@ ros::Timer timer_50hz;
 
 //-----Subscriber
 image_transport::Subscriber sub_frame_bgr;
-image_transport::Subscriber sub_frame_yuv;
+image_transport::Subscriber sub_frame_field_yuv;
 image_transport::Subscriber sub_field_raw_threshold;
 image_transport::Subscriber sub_field_final_threshold;
 image_transport::Subscriber sub_ball_threshold;
@@ -42,10 +42,11 @@ boost::mutex mutex_field_raw_threshold;
 boost::mutex mutex_field_final_threshold;
 boost::mutex mutex_ball_threshold;
 
-Mat frame_bgr = Mat::zeros(g_res_y, g_res_x, CV_8UC3);
+//---Matrix
+//=========
 Mat frame_yuv = Mat::zeros(g_res_y, g_res_x, CV_8UC3);
-Mat frame_yuv_copy = Mat::zeros(g_res_y, g_res_x, CV_8UC3);
-Mat ball_raw_threshold = Mat::zeros(g_res_y, g_res_x, CV_8UC1);
+Mat frame_yuv_field = Mat::zeros(g_res_y, g_res_x, CV_8UC3);
+Mat frame_yuv_ball = Mat::zeros(g_res_y, g_res_x, CV_8UC1);
 Mat field_final_threshold = Mat::zeros(g_res_y, g_res_x, CV_8UC1);
 Mat ball_threshold = Mat::zeros(g_res_y, g_res_x, CV_8UC1);
 Mat obstacle_threshold = Mat::zeros(g_res_y, g_res_x, CV_8UC1);
@@ -55,8 +56,8 @@ uint16_t largest_contours_index = 0;
 
 //---Cam Vars
 //============
-float g_center_cam_x = g_res_x*0.5;
-float g_center_cam_y = g_res_y*0.5;
+float g_center_cam_x = g_res_x * 0.5;
+float g_center_cam_y = g_res_y * 0.5;
 
 //---Ball Vars
 //============
@@ -68,14 +69,14 @@ float g_center_cam_y = g_res_y*0.5;
  * */
 uint16_t g_center_ball_x;
 uint16_t g_center_ball_y;
-uint8_t yuv_thresh[6] = {19,193,0,126,151,255};
+uint8_t yuv_ball_thresh[6] = {19, 193, 0, 126, 151, 255};
+uint8_t yuv_field_thresh[6] = {93,175,127,255,0,98};
 uint8_t g_counter_bola_in;
 uint8_t g_counter_bola_out;
 uint8_t status_bola;
 float g_ball_on_frame_x;
 float g_ball_on_frame_y;
 _Float32x g_ball_on_frame_theta;
-
 
 void CllbkSubFrameBgr(const sensor_msgs::ImageConstPtr &msg);
 void CllbkSubFrameYuv(const sensor_msgs::ImageConstPtr &msg);
@@ -96,7 +97,7 @@ int main(int argc, char **argv)
     //---Subscriber
     //======================
     // sub_frame_bgr = IT.subscribe("/vision_frame_bgr", 32, CllbkSubFrameBgr);
-    sub_frame_yuv = IT.subscribe("/vision_frame_yuv", 32, CllbkSubFrameYuv);
+    sub_frame_field_yuv = IT.subscribe("/vision_yuv", 32, CllbkSubFrameYuv);
     // sub_field_raw_threshold = IT.subscribe("vision_field_raw_threshold", 32, &field_raw_threshold_callback);
     // sub_field_final_threshold = IT.subscribe("vision_field_final_threshold", 32, &field_final_threshold_callback);
     // sub_ball_threshold = IT.subscribe("vision_ball_threshold", 32, &ball_threshold_callback);
@@ -116,106 +117,61 @@ int main(int argc, char **argv)
     // show the p_frame_bgr
     // while (ros::ok())
     // {
-    //     cv::namedWindow("p_frame_bgr", cv::WINDOW_AUTOSIZE);
-    //     cv::imshow("p_frame_bgr", frame_bgr);
+    //     namedWindow("p_frame_bgr", WINDOW_AUTOSIZE);
+    //     imshow("p_frame_bgr", frame_bgr);
     // }
 
     MTS.spin();
 }
 
-// void CllbkSubFrameBgr(const sensor_msgs::ImageConstPtr &msg)
-// {
-// try
-// {
-//     cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-//     cv::waitKey(30);
-// }
-// catch (cv_bridge::Exception &e)
-// {
-//     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-// }
-//     mutex_frame_bgr.lock();
-//     frame_bgr = cv_bridge::toCvShare(msg, "bgr8")->image.clone();
-//     mutex_frame_bgr.unlock();
-// }
-
 void CllbkSubFrameYuv(const sensor_msgs::ImageConstPtr &msg)
 {
-    // try
-    // {
-    //     cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-    //     cv::waitKey(30);
-    // }
-    // catch (cv_bridge::Exception &e)
-    // {
-    //     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-    // }
-    mutex_frame_yuv.lock();
-    frame_yuv = cv_bridge::toCvShare(msg, "bgr8")->image.clone();
-    mutex_frame_yuv.unlock();
+    try
+    {
+        mutex_frame_yuv.lock();
+        frame_yuv = cv_bridge::toCvShare(msg, "bgr8")->image.clone();
+        mutex_frame_yuv.unlock();
+    }
+    catch (cv_bridge::Exception &e)
+    {
+        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    }
 }
 
 void CllbkTim50Hz(const ros::TimerEvent &event)
-{
-    // create trackbar to control YUV
-    cv::namedWindow("view", cv::WINDOW_AUTOSIZE);
-    // cv::createTrackbar("yMin", "view", &yuv_thresh[0], 255);
-    // cv::createTrackbar("yMax", "view", &yuv_thresh[1], 255);
-    // cv::createTrackbar("uMin", "view", &uMin, 255);
-    // cv::createTrackbar("uMax", "view", &uMax, 255);
-    // cv::createTrackbar("vMin", "view", &vMin, 255);
-    // cv::createTrackbar("vMax", "view", &vMax, 255);
+{   
+    //================== 
+    //---Field Threshold
+    //==================
+    Scalar lower_field(yuv_field_thresh[0], yuv_field_thresh[2], yuv_field_thresh[4]);
+    Scalar upper_field(yuv_field_thresh[1], yuv_field_thresh[3], yuv_field_thresh[5]);
 
-    cv::Scalar lower(yuv_thresh[0], yuv_thresh[2], yuv_thresh[4]);
-    cv::Scalar upper(yuv_thresh[1], yuv_thresh[3], yuv_thresh[5]);
+    inRange(frame_yuv, lower_field, upper_field, frame_yuv_field);
 
-    cv::inRange(frame_yuv, lower, upper, ball_raw_threshold);
+    //==================
+    //---Ball Threshold
+    //==================
 
-    // findContours(ball_raw_threshold, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
+    Scalar lower_ball(yuv_ball_thresh[0], yuv_ball_thresh[2], yuv_ball_thresh[4]);
+    Scalar upper_ball(yuv_ball_thresh[1], yuv_ball_thresh[3], yuv_ball_thresh[5]);
 
-    // get the center x and y of ball
-
-    // Mat canny_out;
-    // Canny(ball_raw_threshold, canny_out, 100, 200, 3);
-
-    // std::vector<std::vector<Point>> contours;
-    // findContours(canny_out, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
-
-    // std::vector<std::vector<Point>> contours_poly(contours.size());
-    // std::vector<Rect> boundRect(contours.size());
-    // std::vector<Point2f> centers(contours.size());
-    // std::vector<float> radius(contours.size());
-    // for (size_t i = 0; i < contours.size(); i++)
-    // {
-    //     approxPolyDP(contours[i], contours_poly[i], 3, true);
-    //     boundRect[i] = boundingRect(contours_poly[i]);
-    //     minEnclosingCircle(contours_poly[i], centers[i], radius[i]);
-    // }
-    // Mat drawing = Mat::zeros(canny_out.size(), CV_8UC3);
-    // for (size_t i = 0; i < contours.size(); i++)
-    // {
-    //     Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-    //     drawContours(drawing, contours_poly, (int)i, color);
-    //     circle(drawing, centers[i], (int)radius[i], color, 2);
-    // }
-    // imshow("Contours", drawing);
-    // cv::waitKey(30);
+    inRange(frame_yuv, lower_ball, upper_ball, frame_yuv_ball); 
 
     //=============================
 
     //---Search for ball contours
-    //===========================   
+    //===========================
     std::vector<std::vector<Point>> contours;
     std::vector<Vec4i> hierarchy;
-    findContours(ball_raw_threshold, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    findContours(frame_yuv_ball, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
     //---Get the biggest contour area
     //===============================
     uint16_t largest_area = 0;
     uint16_t largest_contour_index = 0;
 
-    for (uint16_t i = 0; i < contours.size(); i++) 
-    {    
+    for (uint16_t i = 0; i < contours.size(); i++)
+    {
         if (contourArea(contours[i], false) > largest_area)
         {
             largest_area = contourArea(contours[i], false);
@@ -236,7 +192,7 @@ void CllbkTim50Hz(const ros::TimerEvent &event)
     {
         g_counter_bola_in = 0;
         g_counter_bola_out += 17;
-    }    
+    }
 
     //---Found the ball
     //=================
@@ -259,52 +215,34 @@ void CllbkTim50Hz(const ros::TimerEvent &event)
         //---Get the mass centers
         //=======================
         std::vector<Point2f> mc(contours.size());
-        for (uint16_t i = 0; i < contours.size(); i++)    
-            mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);        
+        for (uint16_t i = 0; i < contours.size(); i++)
+            mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
 
         //---Update ball pos
         //==================
-        g_center_ball_x = mc[largest_contour_index].x ;
+        g_center_ball_x = mc[largest_contour_index].x;
         g_center_ball_y = mc[largest_contour_index].y;
 
         //---Ball pos relative from mid of frame
         //======================================
         g_ball_on_frame_x = g_center_ball_x - g_center_cam_x;
         g_ball_on_frame_y = g_center_cam_y - g_center_ball_y;
-        g_ball_on_frame_theta = angles::to_degrees(atan2f32(g_ball_on_frame_y, g_ball_on_frame_x));
+        g_ball_on_frame_theta = atan2f32(g_ball_on_frame_y, g_ball_on_frame_x);
+
+        //---Get Radius
+        //=============
+        static float ball_radius;
+        minEnclosingCircle(contours[largest_contour_index], mc[largest_contour_index], ball_radius);        
 
         //---Draw ball pos
         //================
-        circle(frame_yuv, mc[largest_contour_index], 25, Scalar(0, 0, 255));
+        circle(frame_yuv, mc[largest_contour_index], ball_radius, Scalar(0, 0, 255));
 
         printf("Ball pos: %f | %f | %f\n", g_ball_on_frame_x, g_ball_on_frame_y, g_ball_on_frame_theta);
     }
 
-    // print contour
-    //  for (int i = 0; i < contours.size(); i++)
-    //  {
-    //      for (int j = 0; j < contours[i].size(); j++)
-    //      {
-    //          std::cout << contours[i][j] << std::endl;
-    //      }
-    //  }
-
-    // for(int i = 0; i < contours.size(); i++)
-    // {
-    //     if(contourArea(contours[i]) > largest_contours_area)
-    //     {
-    //         largest_contours_area = contourArea(contours[i]);
-    //         largest_contours_index = i;
-    //     }
-    // }
-    // static Point2f ball_center;
-    // static float ball_radius;
-    // minEnclosingCircle(contours[largest_contours_index], ball_center, ball_radius);
-
-    // circle(frame_yuv, ball_center, ball_radius, Scalar(0, 0, 255), 2, 8, 0);
-
-    cv::imshow("view", ball_raw_threshold);
-    cvtColor(frame_yuv, frame_bgr, cv::COLOR_YUV2BGR);
-    cv::imshow("p_frame_bgr", frame_bgr);
-    cv::waitKey(30);
+    imshow("view ball", frame_yuv_ball);
+    imshow("view field", frame_yuv_field);
+    imshow("view yuv", frame_yuv);
+    waitKey(30);
 }
